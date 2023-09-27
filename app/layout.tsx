b2f67ccd378936f6e55418969314716b2d07aeb9
임빛나 <devbit4@gmail.com>
2023-09-27 13:19:16 +0900

    Design: Header, Wrapper 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
+import StyledJsxRegistry from './registry'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import Header from './components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +22,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledJsxRegistry>
+          <Header />
+          {children}
+        </StyledJsxRegistry>
+      </body>
     </html>
   )
 }

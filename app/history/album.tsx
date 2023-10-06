a8a0ecfab559bf9ffa7c83d9f78d611e5e8aaf9f
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-10-06 16:40:30 +0900

    Design:앨범 사진,비디오 사이즈 일정하게 변경

--- app/history/album.tsx
+++ app/history/album.tsx
@@ -1,45 +1,55 @@
 import React from 'react'
 
 const Album = ({ images }) => {
+  console.log(images)
   return (
     <div className="Album">
       <div className="album-grid">
         {images.length > 0 &&
-          images.map((content, index: number) => (
+          images.map((content, index) => (
             <div key={index} className="content-item">
-              {content.type.startsWith('image/') ? (
-                <img
-                  src={URL.createObjectURL(content)}
-                  alt={`Image ${index}`}
-                  style={{ maxWidth: '100%', maxHeight: '200px' }}
-                />
-              ) : content.type.startsWith('video/') ? (
-                <video
-                  controls
-                  src={URL.createObjectURL(content)}
-                  alt={`Video ${index}`}
-                  style={{ maxWidth: '100%', maxHeight: '200px' }}
-                />
+              {content.type.startsWith('image/') ||
+              content.type.startsWith('video/') ? (
+                <div className="media-container">
+                  {content.type.startsWith('image/') ? (
+                    <img
+                      src={URL.createObjectURL(content)}
+                      alt={`Image ${index}`}
+                    />
+                  ) : content.type.startsWith('video/') ? (
+                    <video
+                      controls
+                      src={URL.createObjectURL(content)}
+                      alt={`Video ${index}`}
+                    />
+                  ) : null}
+                </div>
               ) : null}
             </div>
           ))}
       </div>
       <style jsx>{`
         .Album {
-          /* Add styles for the Album container */
         }
 
         .album-grid {
           display: grid;
-          grid-template-columns: repeat(
-            4,
-            1fr
-          ); /* Adjust the number of columns as needed */
+          grid-template-columns: repeat(4, 1fr);
           gap: 10px;
         }
 
+        .media-container {
+          max-height: 200px;
+          max-width: 200px;
+          overflow: hidden;
+        }
+
+        .media-container img,
+        .media-container video {
+          width: 100%;
+          height: 100%;
+        }
         .content-item {
-          /* Add styles for each content item */
         }
       `}</style>
     </div>

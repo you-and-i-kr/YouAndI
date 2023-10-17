4987ae61a13028ab3404ede55c57a6e1edf4825f
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-10-17 18:05:01 +0900

    Feat:콘텐츠삭제 기능 추가

--- app/history/popup.tsx
+++ app/history/popup.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import AlbumComment from './comment'
 
 interface AlbumPopupProps {
@@ -6,14 +8,20 @@ interface AlbumPopupProps {
   setContentClicked: (value: boolean) => void
   comments: Comment[]
   setComments: (comments: Comment[]) => void
+  onDelete: () => void
 }
 
 const AlbumPopup: React.FC<AlbumPopupProps> = ({
+  onDelete,
   contents,
   setContentClicked,
   comments,
   setComments,
 }) => {
+  const [deleteContent, setDeleteContent] = useState(false)
+  const deleteHandler = () => {
+    setDeleteContent(!deleteContent)
+  }
   return (
     <div className="AlbumPopup">
       <div className="popup-content">
@@ -34,9 +42,27 @@ const AlbumPopup: React.FC<AlbumPopupProps> = ({
           setComments={setComments}
         />
       </div>
+      <div className="content-delete-con">
+        <button
+          className="content-delete-box"
+          onClick={() => {
+            deleteHandler()
+          }}
+        >
+          :
+        </button>
+        {deleteContent ? (
+          <button className="content-delete-btn" onClick={onDelete}>
+            삭제
+          </button>
+        ) : (
+          ''
+        )}
+      </div>
       <style jsx>
         {`
           .AlbumPopup {
+            position: relative;
             display: flex;
             align-items: center;
             justify-content: center;
@@ -80,8 +106,27 @@ const AlbumPopup: React.FC<AlbumPopupProps> = ({
             justify-content: flex-start;
             margin-left: 20px;
           }
-
-          .popup-btn {
+          .content-delete-con {
+            position: absolute;
+            top: 120px;
+            right: 300px;
+          }
+          .content-delete-box {
+            font-weight: 700;
+            font-size: 20px;
+            color: white;
+            border: none;
+            background-color: transparent;
+          }
+          .content-delete-btn {
+            position: absolute;
+            top: 5px;
+            left: 30px;
+            width: 50px;
+            border: none;
+            border-radius: 5px;
+            color:black
+            background-color: #ecedf1;
           }
         `}
       </style>

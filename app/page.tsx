85cc24b2709b310001db5cea364622ddc543f53d
임빛나 <devbit4@gmail.com>
2023-11-04 15:54:16 +0900

    Fix : 충돌해결

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 import Wrapper from './components/Wrapper'
 import axios from 'axios'
